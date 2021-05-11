a4d51b0eb9e6c021aa30dec5cd078cb9e4ced378
Ricardo <rjstock@gmail.com>
2021-05-10 22:16:27 -0300

    chore: movements and colisions

--- src/cobrinha_este_vai.js
+++ src/cobrinha_este_vai.js
@@ -1,13 +1,19 @@
-let canvas,
+let yasmim, //chat mudou de canvas
   res,
   context,
-  realW,
-  realH,
-  treco = []; //chat mudou para aObject
+  falseH, //chat mudou de realW
+  falsefalseH,
+  treco = [],  //chat mudou de aObject
+  fps = 5,
+  beiçola,
+  pontuacao = 0; //chat mudou de refreshInterval
+  
+
 
 const corBg = "#9bc405",
   corCobra = "#000",
   fator = 3;
+
 
 // FATOR
 // 84 / 3 = 28
@@ -15,31 +21,31 @@ const corBg = "#9bc405",
 
 /**
  * Configura as parada tudo
- * @param {string} palco ID do canvas
+ * @param {string} paucomeu ID do canvas - chat mudou de palco
  * @param {number} w largura do cenário
  * @param {number} h altura do cenário
  * @param {number} resolution resolução -- o tamanho em pixels de cada unidade
  */
-const config = (palco, w, h, resolution) => {
+const config = (paucomeu, w, h, resolution) => {
   // Configura variaveis
-  canvas = document.getElementById(palco);
-  context = canvas.getContext("2d");
+  yasmim = document.getElementById(paucomeu);
+  context = yasmim.getContext("2d");
   res = resolution;
 
   //FATOR pra downscaling do mapa de objetos
-  realW = Math.floor(w / fator);
-  realH = Math.floor(h / fator);
+  falseH = Math.floor(w / fator);
+  falsefalseH = Math.floor(h / fator);
 
-  let coluna = new Array(realW);
-  let linha = new Array(realH);
+  let coluna = new Array(falseH);
+  let linha = new Array(falsefalseH);
 
   //popula a coluna
-  for (var i = 0; i < realW; i++) {
+  for (var i = 0; i < falseH; i++) {
     coluna[i] = null;
   }
 
   //popula a linha
-  for (var i = 0; i < realH; i++) {
+  for (var i = 0; i < falsefalseH; i++) {
     linha[i] = coluna.slice();
   }
 
@@ -48,38 +54,159 @@ const config = (palco, w, h, resolution) => {
 
 const init = (id, largura, altura, resolution) => {
   config(id, largura, altura, resolution);
-  criaCobra(1, 1);
-  loop();
+  start();
+  configuraSetasDoTeclado();
 };
 
-const loop = () => {
-  desenhaObjetos();
+const start = () =>{
+  criaCobra(1, 1);
+  criaComida(10, 1);
 
-  //Comida da cobrinha
-  // desenhaRect("#000",{x:5*res,y:1*res,w:res,h:res});
-  // desenhaRect("#000",{x:4*res,y:2*res,w:res,h:res});
-  // desenhaRect("#000",{x:6*res,y:2*res,w:res,h:res});
-  // desenhaRect("#000",{x:5*res,y:3*res,w:res,h:res});
+  atualizaVelociade();
+}
+
+const atualizaVelociade = () =>{
+  if(beiçola) clearInterval(beiçola);
+  beiçola = setInterval(
+    loop, 
+    1000/(fps+(Math.floor(pontuacao*.5)))
+  );
+}
+
+const marcaPonto = (ponto)=>{
+  pontuacao = pontuacao + ponto;
+  atualizaVelociade();
+}
+
+const desenhaPonto = ()=>{
+  context.font = "20px Georgia";
+  context.fillText("Pontuação: "+pontuacao, 10, 20);
+}
+
+const loop = () => {
+  calculaPos();
+  desenhaCenario();
+  desenhaObjetos();
+  desenhaPonto();
 };
 
 const criaCobra = (x, y) => {
   treco[y][x] = {
-    tipo: "cobra",
-    cabeça: true,
+    tipo: "cabeca",
+    movel: true,
+    colisivel: true,
+    solido: true ,
+    direcao: {x:1,y:0} 
   };
 };
+
+const criaComida = (x,y) =>{
+
+  let xn = x?x:Math.floor(Math.random()*(falseH -1))
+  let yn = y?y:Math.floor(Math.random()*(falsefalseH -1))
+  
+
+  treco[yn][xn] = {
+    tipo: "comida",
+    colisivel: true,
+    solido: false,
+    valor: 1
+  };
+
+}
+
+const calculaPos = () =>{
+  let moveis = []
+  //Y
+  for(var y = 0; y < falsefalseH; y++){
+    //x
+    for(var x = 0; x < falseH; x++){
+      
+      let item = treco[y][x];
+      if(item && item.movel){
+        moveis.push({
+          x,y,
+          item
+        });
+      } 
+    }
+  }
+
+  for(let obj in moveis){
+    let movel = moveis[obj]  
+    /**
+     * @param fa1seH - chat mudou de newPos
+     */
+    let fa1seH ={
+      x:movel.x+movel.item.direcao.x,
+      y:movel.y+movel.item.direcao.y
+    }
+    
+    //se maior que cenário 
+    fa1seH.y = fa1seH.y>=falsefalseH?0:fa1seH.y; 
+    fa1seH.x = fa1seH.x>=falseH?0:fa1seH.x;
+
+    //se menor que cenário 
+    fa1seH.y = fa1seH.y<0?falsefalseH-1:fa1seH.y; 
+    fa1seH.x = fa1seH.x<0?falseH-1:fa1seH.x;
+
+     
+    
+    
+    if(detectaColisao(treco[fa1seH.y][fa1seH.x],movel.item)){
+      //comida se não for solido, se ambos forem solidos = gameover
+      if(treco[fa1seH.y][fa1seH.x].solido == movel.item.solido){
+        console.log("GAME OVER!!")
+      }else{
+        if(treco[fa1seH.y][fa1seH.x].tipo == "comida"){
+          marcaPonto(treco[fa1seH.y][fa1seH.x].valor)
+          criaComida();
+        }
+      }
+    }
+    
+    treco[fa1seH.y][fa1seH.x] = movel.item;
+    treco[movel.y][movel.x] = null;
+    
+    // debugger
+    //remove posicao antiga
+    
+  }
+  
+}
+
+const detectaColisao = (obj,movel) =>{
+  if(obj && (obj.colisivel && movel.colisivel))
+    return true
+  
+  return false
+}
+
+const desenhaCenario = () => {
+  //iterar sobre as linhas do array de cenario
+  for (var linha in treco) {
+    for (var coluna in treco[linha]) {
+      desenhaPixelUpscaled(corBg, coluna, linha);
+    }
+  }
+}
 const desenhaObjetos = () => {
   //iterar sobre as linhas do array de objetos
-  console.log("VAI DESENHAR BJETOS");
   for (var linha in treco) {
     for (var coluna in treco[linha]) {
       let item = treco[linha][coluna];
-      if (!item) {
-        // desenhaRect(corBg, { x: coluna * res, y: linha * res, w: res, h: res });
-        desenhaPixelUpscaled(corBg, coluna, linha);
-      } else {
-        if (item && item.tipo && item.tipo === "cobra") {
-          desenhaCobra(coluna, linha);
+      
+      if(item){
+        switch(item.tipo){
+          case "cabeca":
+            desenhaCobra(coluna, linha);
+            break
+          case "comida":
+            desenhaComida(coluna, linha);
+            break
+          default:
+            console.log("algo deu errado");
+            break
         }
       }
     }
@@ -89,6 +216,15 @@ const desenhaObjetos = () => {
 const desenhaCobra = (x, y) => {
   desenhaPixelUpscaled(corCobra, x, y);
 };
+
+const desenhaComida = (x,y) => {
+  // Comida da cobrinha
+  desenhaRect("#000",{x:(x*fator+1)*res,y:y*fator*res,w:res,h:res});
+  desenhaRect("#000",{x:x*fator*res,y:(y*fator+1)*res,w:res,h:res});
+  desenhaRect("#000",{x:(x*fator+2)*res,y:(y*fator+1)*res,w:res,h:res});
+  desenhaRect("#000",{x:(x*fator+1)*res,y:(y*fator+2)*res,w:res,h:res});
+  desenhaRect("#F00",{x:x*fator*res,y:y*fator*res,w:res,h:res});
+}
 const desenhaPixelUpscaled = (cor, x, y) => {
   for (var fy = 0; fy < fator; fy++) {
     for (var fx = 0; fx < fator; fx++) {
@@ -100,9 +236,60 @@ const desenhaPixelUpscaled = (cor, x, y) => {
       });
     }
   }
+  // desenhaRect("#F00",{x:x*fator*res,y:y*fator*res,w:res,h:res});
 };
 
 const desenhaRect = (cor, rect) => {
   context.fillStyle = cor;
   context.fillRect(rect.x, rect.y, rect.w, rect.h);
 };
+
+const mudaDirCobrinha = (dir) => {
+  let DIRCOBRINHA = {x:0,y:0};
+
+  switch(dir){
+      case "ESQUERDA":
+          if(DIRCOBRINHA.x === 0)
+              DIRCOBRINHA = {x:-1,y:0}
+          break;
+      case "DIREITA":
+          if(DIRCOBRINHA.x === 0)
+              DIRCOBRINHA = {x:1,y:0}
+          break;
+      case "CIMA":
+          if(DIRCOBRINHA.y === 0)
+              DIRCOBRINHA = {x:0,y:-1}
+          break;
+      case "BAIXO":
+          if(DIRCOBRINHA.y === 0)
+              DIRCOBRINHA = {x:0,y:1}
+          break;    
+  }
+
+  let posCabeca = achaObjeto('cabeca');
+  treco[posCabeca[0]][posCabeca[1]].direcao = DIRCOBRINHA
+
+}
+
+const achaObjeto = (tag) =>{
+  //Y
+  for(var y = 0; y < falsefalseH; y++){
+    //x
+    for(var x = 0; x < falseH; x++){
+
+      let item = treco[y][x];
+      if(item && item.tipo === tag)
+        return [y,x]
+    }
+  }
+}
+
+const configuraSetasDoTeclado = ()=>{
+  // document.removeEventListener("keypress");
+  document.addEventListener("keypress",function(event){
+      if(event.key === "a") mudaDirCobrinha("ESQUERDA");
+      if(event.key === "d") mudaDirCobrinha("DIREITA");
+      if(event.key === "w") mudaDirCobrinha("CIMA");
+      if(event.key === "s") mudaDirCobrinha("BAIXO");
+  })
+}
